96020190e7a09144af98060b98aeee6da8df18a0
unknown <krasimir.strashimirov@outlook.com>
2022-04-08 19:33:47 +0300

    Middleware added in settings.py

--- travel/travel/settings.py
+++ travel/travel/settings.py
@@ -41,6 +41,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'travel.middlewares.handle_exception',
 ]
 
 ROOT_URLCONF = 'travel.urls'
